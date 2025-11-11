46b2a50a2f350a89b30e1b24361ace1a727310ba
mihirchaple005 <mihirchaple005@gmail.com>
2025-11-11 18:44:07 +0530

    adding database service

---
+++ DatabaseMicroservice/src/test/java/org/vibedaily/databasemicroservice/DatabaseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vibedaily.databasemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
